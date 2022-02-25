8d953d1c6e4d70a2cede093161ef324cd97d3182
Ampeire <ampeireedgar5@gmail.com>
2022-02-25 13:08:00 +0300

    Initial Commit

---
+++ Datero.php
@@ -0,0 +1,19 @@
+<?php
+
+
+/**
+ * @package Datero
+ * @author Ampeire Edgar
+ * @license GPL V2 or later
+ */
+
+/*
+ * Plugin Name: Datero
+ * Plugin URI: https://github.com/dancan-edgar/egosms.git
+ * Description: Datero enables use of date shotcodes.
+ * Version: 1.0.0
+ * Author: Ampeire Edgar
+ * Author URI: https://github.com/dancan-edgar
+ * License: GPL V2 or higher
+ * Text Domain: egosms
+ */
